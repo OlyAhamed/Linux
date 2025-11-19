ac7296ec9449d5450f23c06506aa7795ecdade59
OlyAhamed <olykhan1056@gmail.com>
2025-11-19 08:42:10 +0600

    Delete .config/scripts directory

--- .config/scripts/poweroff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo " "   # power icon (FontAwesome)
-if [[ $BLOCK_BUTTON == 1 ]]; then
-    systemctl poweroff
-fi
-
--- .config/scripts/restart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo " "   # restart icon (FontAwesome)
-if [[ $BLOCK_BUTTON == 1 ]]; then
-    systemctl reboot
-fi
-
