fd05941ff4708fb2003432ea5fae42376a1e31a7
OlyAhamed <olykhan1056@gmail.com>
2026-01-20 09:52:39 +0600

    Delete Sway-Config/eww directory

--- Sway-Config/eww/scripts/disconnect-wifi.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-iwctl station wlan0 disconnect
-notify-send "WiFi" "Disconnected"
--- Sway-Config/eww/scripts/get_media.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Real-time media listener for eww
-playerctl --follow metadata --format \
-'{"status": "{{status}}", "title": "{{title}}", "artist": "{{artist}}", "art": "{{mpris:artUrl}}", "time": "{{duration(position)}}", "total": "{{duration(mpris:length)}}", "perc": "{{(position/mpris:length)*100}}"}' 2>/dev/null
