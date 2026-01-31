0e252d67331d0bbdf5b494d658989f85746d9bfc
OlyAhamed <olykhan1056@gmail.com>
2026-01-31 12:17:23 +0600

    Add files via upload

---
+++ DotFiles/TopBars/waybar/hypr-waybar/launch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+killall waybar
+waybar -c ~/.config/waybar/hypr-waybar/config.jsonc -s ~/.config/waybar/hypr-waybar/style.css
+
---
+++ DotFiles/TopBars/waybar/sway-bar/launch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+killall waybar
+waybar -c ~/.config/waybar/sway-bar/config.jsonc -s ~/.config/waybar/sway-bar/style.css
+
+
